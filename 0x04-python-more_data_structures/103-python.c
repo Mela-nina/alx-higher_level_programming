c42bc018f2c0cd85292631c0546aa064bd1cd139
Mela-nina <oge4sym@gmail.com>
2023-06-14 14:35:29 -0700

    CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -6,13 +6,14 @@
  * @o: This is the python object
  * Return: no return
  */
+
 void print_python_bytes(PyObject *o)
 {
 	char *string;
-	long int size, j, limit;
+	long int size, o, limit;
 
 	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(p))
+	if (!PyBytes_Check(o))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
@@ -31,11 +32,11 @@ void print_python_bytes(PyObject *o)
 
 	printf("  first %ld bytes:", limit);
 
-	for (j = 0; j < limit; j++)
-		if (string[j] >= 0)
-			printf(" %02x", string[j]);
+	for (i = 0; i < limit; i++)
+		if (string[i] >= 0)
+			printf(" %02x", string[i]);
 		else
-			printf(" %02x", 256 + string[j]);
+			printf(" %02x", 256 + string[i]);
 
 	printf("\n");
 }
@@ -45,9 +46,10 @@ void print_python_bytes(PyObject *o)
  * @o: This is the python object
  * Return: no return
  */
+
 void print_python_list(PyObject *o)
 {
-	long int size, j;
+	long int size, i;
 	PyListObject *list;
 	PyObject *obj;
 
@@ -58,10 +60,10 @@ void print_python_list(PyObject *o)
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
-		obj = ((PyListObject *)o)->ob_item[j];
-		printf("Element %ld: %s\n", j, ((obj)->ob_type)->tp_name);
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 	}
