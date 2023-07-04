32478ac4df52c410c2d076958c7ba1419dc3bb2c
Mela-nina <oge4sym@gmail.com>
2023-07-04 04:38:49 -0700

    Task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -58,7 +58,10 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self) -> str:
-        """This will present a diagram of the rectangle defined for an object"""
+        """
+        This will present a diagram of the rectangle
+        defined for an object
+        """
         if self.__width == 0 or self.__height == 0:
             return ("")
         rectangle = ""
