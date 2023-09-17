d7c363dd73a9b952323dc91739a8ab26eb1a7c5e
Mela-nina <oge4sym@gmail.com>
2023-09-17 21:50:52 +0000

    0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This is the script that defines a City class
+to work with MySQLAlchemy ORM
+"""
+
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """City class
+
+    Attributes:
+        __tablename__ (str): This is the table name of the class
+        id (int): This is the id of the class
+        name (str): This is the name of the class
+        state_id (int): This is the state the city belongs to
+
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
